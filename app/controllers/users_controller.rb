4fb60f11610cf32393d3f3395d3ae8c910b4f641
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-01-23 21:58:54 +0100

    Display validation errors in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Your account was successfuly created"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Something went wrong. Plese review the form."
+      render 'new'
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
