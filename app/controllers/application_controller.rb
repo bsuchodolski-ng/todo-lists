515d87434bfef37cbd7182e5bea66178901cc182
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-02-09 14:15:19 +0100

    Extract logged_in filter to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  private
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in and try again.'
+        redirect_to login_path
+      end
+    end
 end
