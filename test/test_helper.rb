55bb5bfcc658c83514827288dd6dda499dfebafd
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-02-23 10:37:31 +0100

    Clean up test_helper a bit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,26 +3,24 @@ require 'rails/test_help'
 require 'capybara/rails'
 require 'capybara/minitest'
 
-class ActionDispatch::IntegrationTest
-  # Make the Capybara DSL available in all integration tests
-  include Capybara::DSL
-  # Make `assert_*` methods behave like Minitest assertions
-  include Capybara::Minitest::Assertions
-
-  # Reset sessions and driver between tests
-  # Use super wherever this method is redefined in your individual test classes
-  def teardown
-    Capybara.reset_sessions!
-    Capybara.default_driver = :webkit
-    Capybara.use_default_driver
-  end
-end
-
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
 end
 
 class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+  # Set webkit as default driver
+  Capybara.default_driver = :webkit
+
+  # Reset sessions and driver between tests
+  # Use super wherever this method is redefined in your individual test classes
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 
   def log_in_as(user)
     post login_path, params: {
