ea77f413690fe415fc79b5ea275854a411186361
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-03-14 21:04:09 +0100

    Add missing tests for users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,42 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+  setup do
+    @user = create(:user)
+  end
+
   test 'should get new' do
     get signup_path
     assert_response :success
   end
 
-  test 'cant update when not logged in' do
+  test 'should create user' do
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {
+        user: {
+          email: 'user@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+    assert_not flash.empty?
+    assert_redirected_to root_path
+  end
+
+  test 'edit should redirect to login if not logged in' do
+    get account_path
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
+  test 'should get edit if logged in' do
+    log_in_as(@user)
+    get account_path
+    assert_response :success
+  end
+
+  test 'update should redirect to login if not logged in' do
     patch account_path, params: {
       user: {
         email: 'user@example.com',
@@ -19,9 +49,29 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
-  test 'cant delete when not logged in' do
+  test 'update is possible for logged in user' do
+    log_in_as(@user)
+    patch account_path, params: {
+      user: {
+        email: 'user@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    }
+    assert_not flash.empty?
+    assert @user.reload.email == 'user@example.com'
+  end
+
+  test 'destroy should redirect to login' do
     delete account_path
     assert_not flash.empty?
     assert_redirected_to login_path
+  end
+
+  test 'should destroy user if user is logged in' do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete account_path
+    end
   end
 end
