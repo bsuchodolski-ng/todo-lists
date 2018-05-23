e3c2503468874f108b812a46d6a80fa45f80b68f
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-05-23 15:21:25 +0200

    allow to add to do list with no title

--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -1,6 +1,6 @@
 class ToDoList < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :user, presence: true
   has_many :to_do_list_items, dependent: :destroy
 end
